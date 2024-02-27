47b8c22feafb905337f06a78607c9bfbcca20633
rustem <230324@astanait.edu.kz>
2024-02-27 20:52:20 +0600

    0.8

---
+++ src/main/java/com/finctonrol/financialcontrol/services/TransactionService.java
@@ -0,0 +1,46 @@
+//Комментарий преподавателю: Я, Рустем Алигожин, удалил репозиторий и создал новый такой же,
+//поэтому слетели коммиты мои, по этой причине я перезаливаю весь процесс создания проекта
+//Также изначально начинали делать на спринге.
+
+package com.finctonrol.financialcontrol.services;
+
+import com.finctonrol.financialcontrol.models.TransactionEntity;
+import com.finctonrol.financialcontrol.models.UserEntity;
+import com.finctonrol.financialcontrol.repositories.TransactionRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final UserService userService;
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, UserService userService){
+        this.transactionRepository = transactionRepository;
+        this.userService = userService;
+    }
+
+    @Transactional
+    public TransactionEntity addTransaction(TransactionEntity transaction){
+        return transactionRepository.save(transaction);
+    }
+
+    public List<TransactionEntity> getAllTransactions(){
+        return transactionRepository.findAll();
+    }
+
+    public List<TransactionEntity> getTransactionByUsername(String username){
+        UserEntity user = userService.getUserByUsername(username);
+        return transactionRepository.findByUserEntity(user);
+    }
+
+    public void deleteTransaction(Long id){
+        transactionRepository.deleteById(id);
+    }
+
+}
