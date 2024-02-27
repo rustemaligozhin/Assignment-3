58cceb96f07a98b8db11670b3d88b9a1b47864ef
rustem <230324@astanait.edu.kz>
2024-02-27 20:49:58 +0600

    0.6

---
+++ src/main/java/com/finctonrol/financialcontrol/controllers/UserController.java
@@ -0,0 +1,57 @@
+//Комментарий преподавателю: Я, Рустем Алигожин, удалил репозиторий и создал новый такой же,
+//поэтому слетели коммиты мои, по этой причине я перезаливаю весь процесс создания проекта
+//Также изначально начинали делать на спринге.
+
+package com.finctonrol.financialcontrol.controllers;
+
+import com.finctonrol.financialcontrol.exception.UserExistException;
+import com.finctonrol.financialcontrol.models.UserEntity;
+import com.finctonrol.financialcontrol.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+    @GetMapping
+    public String users(@RequestParam(name = "username", required = false) String username, Model model){
+        model.addAttribute("users", userService.listUsers(username));
+        return "users";
+    }
+
+    @PostMapping
+    public ResponseEntity createUser(@Valid @RequestBody UserEntity user){
+        try {
+            userService.createUser(user);
+            return ResponseEntity.ok("User created");
+        }catch (UserExistException uee){
+            return ResponseEntity.badRequest().body(uee.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("ERROR");
+        }
+
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id){
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.ok("User deleted");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Failed to delete user" + e.getMessage());
+        }
+    }
+
+
+
+}
