a50bd452a1818d2bce2b88dbcaca001cb560b8ba
rustem <230324@astanait.edu.kz>
2024-02-27 20:30:57 +0600

    0.1

---
+++ src/main/java/com/finctonrol/financialcontrol/FinancialcontrolApplication.java
@@ -0,0 +1,18 @@
+//Комментарий преподавателю: Я, Рустем Алигожин, удалил репозиторий и создал новый такой же,
+//поэтому слетели коммиты мои, по этой причине я перезаливаю весь процесс создания проекта
+//Также изначально начинали делать на спринге.ы
+
+
+package com.finctonrol.financialcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialcontrolApplication.class, args);
+	}
+
+}
